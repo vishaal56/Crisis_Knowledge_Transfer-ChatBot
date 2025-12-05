a88e1654e538a9830088780a251a0d919ce4d44f
sairahul <sairahulvm@gmail.com>
2025-12-06 00:14:56 +0100

    first commit of sai

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class CrisisChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Crisis Knowledge Assistant',
+      title: 'Crisis Knowledge Assistan',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: 'Roboto',
